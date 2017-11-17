d13e505043fd25a04939f8ec6ba7c95aaf4eaea5
Xavi Torelló <info@xaviertorello.cat>
2017-11-17 15:09:35 +0100

    Initial package version

---
+++ spec/git-projects-list-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GitProjectsListView from '../lib/git-projects-list-view';
+
+describe('GitProjectsListView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
