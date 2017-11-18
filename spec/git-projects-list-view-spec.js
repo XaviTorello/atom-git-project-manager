21c277bce68b3a78bb57967c157e550e4931b677
Xavi Torelló <info@xaviertorello.cat>
2017-11-18 10:47:34 +0100

    Delete non-used view

--- spec/git-projects-list-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import GitProjectsListView from '../lib/git-projects-list-view';
-
-describe('GitProjectsListView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
